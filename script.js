b6b39fe27f29caac025880fd9c0d8881dac26e73
Avinasen <115348427+kitteneditorc0der@users.noreply.github.com>
2022-12-30 16:48:40 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+var editor = ace.edit("editor");
+var result = document.getElementById("result").contentWindow.document;
+editor.setTheme("ace/theme/light");
+editor.session.setMode("ace/mode/html");
+editor.setValue("<!DOCTYPE html>\n<html>\n\t<head>\n\t\t<title>New Website</title>\n\t\t<meta charset=\"utf-8\">\n\t</head>\n\t<body>\n\t\tHello world!\n\t</body>\n</html>");
+function showRes() {
+  result.open();
+  result.writeln(editor.getValue());
+  result.close();
+}
+editor.session.on("change", showRes);
+showRes();
+function download(filename, text) {
+  var element = document.createElement("a");
+  element.setAttribute("href", "data:text/plain;charset=utf-8," + encodeURIComponent(text));
+  element.setAttribute("download", filename);
+  element.style.display = "none";
+  document.body.appendChild(element);
+  element.click();
+  document.body.removeChild(element);
+}
+document.getElementById("save").addEventListener("click", function() {
+  download("program.html", editor.getValue());
+});

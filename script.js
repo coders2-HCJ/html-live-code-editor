13740371f14903b38ebb6ec176c1d093826f5a21
Avinasen <115348427+coders2-HCJ@users.noreply.github.com>
2023-01-10 14:21:35 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ editor.setTheme("ace/theme/light");
 editor.session.setMode("ace/mode/html");
 editor.setValue("<!DOCTYPE html>\n<html>\n\t<head>\n\t\t<title>New Website</title>\n\t\t<meta charset=\"utf-8\">\n\t</head>\n\t<body>\n\t\tHello world!\n\t</body>\n</html>");
 function showRes() {
-  result.src = "data:text/html;charset=utf-8," + encodeURIComponent(editor.getValue());
+	result.src = "data:text/html;charset=utf-8," + encodeURIComponent(editor.getValue());
 }
 editor.session.on("change", showRes);
 showRes();
